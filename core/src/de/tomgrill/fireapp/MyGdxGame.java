b440bb9b3c71b8f2bdfb1b5adc69ce4cad560127
Thomas Pronold <tp@tpronold.de>
2017-10-04 12:48:25 +0200

    basic setup - bindings throw error

--- core/src/de/tomgrill/fireapp/MyGdxGame.java
+++ core/src/de/tomgrill/fireapp/MyGdxGame.java
@@ -2,11 +2,16 @@ package de.tomgrill.fireapp;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import mk.gdx.firebase.GdxFIRApp;
+import mk.gdx.firebase.GdxFIRAuth;
+import mk.gdx.firebase.auth.GdxFirebaseUser;
+import mk.gdx.firebase.callbacks.AuthCallback;
 
-public class MyGdxGame extends ApplicationAdapter {
+public class MyGdxGame extends ApplicationAdapter implements InputProcessor {
 	SpriteBatch batch;
 	Texture img;
 	
@@ -15,7 +20,9 @@ public class MyGdxGame extends ApplicationAdapter {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 
+        GdxFIRApp.instance().configure();
 
+        Gdx.input.setInputProcessor(this);
 	}
 
 	@Override
@@ -32,4 +39,55 @@ public class MyGdxGame extends ApplicationAdapter {
 		batch.dispose();
 		img.dispose();
 	}
+
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        GdxFIRAuth.instance().signInAnonymously(new AuthCallback() {
+            @Override
+            public void onSuccess(GdxFirebaseUser user) {
+                System.out.println(user.getUserInfo().getUid());
+            }
+
+            @Override
+            public void onFail(Exception e) {
+                System.out.println(e.getMessage());
+            }
+        });
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
 }
